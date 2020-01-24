b4ed1269f6e0b45d9f061a9121a14f90aeb2609b
marzieh <marziehseyedmoosavi@gmail.com>
2020-01-23 22:33:47 -0800

    chng1 diagram

--- src/slr/diagram.java
+++ src/slr/diagram.java
@@ -5,10 +5,36 @@
  */
 package slr;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author marzieh
  */
 public class diagram {
-    
+    ArrayList makeDiagram (ArrayList<Rule> rules){
+        ArrayList states ,my_rules;
+        int i ;
+        Character dot = new Character('.', false, false);
+        Rule currentRule = rules.get(0);
+        i = 0;
+        currentRule.setDotPlace(i);
+        if(currentRule.getRight().get(i).get){
+            
+        }
+        
+        
+        
+        
+        
+        //State s =  new State(my_rules);
+       
+        
+        
+        return states;
+    }
+
+    private boolean checkAfterDot(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
